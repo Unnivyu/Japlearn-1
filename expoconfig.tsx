75e0a17cfd2e2262ee850227e024419ae5d4239a
unknown <kerryambos884512345@gmail.com>
2024-09-09 03:57:14 +0800

    email conf p2

--- expoconfig.tsx
+++ expoconfig.tsx
@@ -2,6 +2,7 @@ import Constants from 'expo-constants';
 
 const { manifest } = Constants;
 let localhost = 'http://localhost:8080'; // Default to localhost
+const productionBackendUrl = 'https://backend-omega-pink.vercel.app/api'; // Your production backend URL
 
 if (manifest && manifest.debuggerHost) {
     localhost = `http://${manifest.debuggerHost.split(':').shift()}:8080`;
@@ -11,10 +12,11 @@ const ipProvided = 'http://192.168.1.5:8080'; // Your provided IP address
 
 // Function to determine the API URL
 const getApiUrl = () => {
-    // Add any custom logic to choose between IP or localhost
-    const useProvidedIp = true; // Set this condition as needed
+    // Add any custom logic to choose between IP or localhost for development
+    const isProduction = process.env.NODE_ENV === 'production'; // Determine if in production environment
+    const useProvidedIp = !isProduction; // Use provided IP in development
 
-    return useProvidedIp ? ipProvided : localhost;
+    return isProduction ? productionBackendUrl : (useProvidedIp ? ipProvided : localhost);
 };
 
 const expoconfig = {
